9dbdc69ab64812bbb18ee0e41cc0692cdd945852
main0614 <wdqkm409@gmail.com>
2019-05-17 17:01:55 +0900

    基本情報を追加

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,8 +1,9 @@
 class LunchController < ApplicationController
   def go
 
-    eataries = ['松のや', 'あすかて食堂', 'te.to.te', 'トタンコットンカフェ', '711で弁当']
+    eataries = Eatery.all
 
     @recommend = eataries.sample
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Eatery.create(name: "松のや つくば東大通り店", zip: "305-0005",
+              address: "茨城県つくば市天久保4丁目6-8", tel: "029-850-0781")
+
+Eatery.create(name: "あすかて食堂", zip: "305-0005",
+              address: "茨城県つくば市天久保2丁目10-2", tel: "")
+
+Eatery.create(name: "te.to.te", zip: "305-0003",
+              address: "茨城県つくば市桜2丁目1-7 グランパスつくば102号", tel: "029-857-7793")
+
+Eatery.create(name: "トタンコットンカフェ", zip: "305-0005",
+              address: "茨城県つくば市天久保4丁目5-17", tel: "029-811-6206")
+
+Eatery.create(name: "セブン-イレブン つくば松見公園店", zip: "305-0005",
+              address: "茨城県つくば市天久保1丁目10-25", tel: "029-852-3030")
